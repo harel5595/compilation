78df608af6697869e55a5bcb249d63d9f4e58db6
unknown <Khashper@mail.tau.ac.il>
2021-10-31 15:24:54 +0200

    adde all tokens

--- ex1/src/TokenNames.java
+++ ex1/src/TokenNames.java
@@ -7,9 +7,31 @@ public interface TokenNames {
   public static final int DIVIDE = 4;
   public static final int LPAREN = 5;
   public static final int RPAREN = 6;
-  public static final int LBRACK = 9;
-  public static final int RBRACK = 10;
   public static final int NUMBER = 7;
   public static final int ID = 8;
+
+  public static final int LBRACK = 9;
+  public static final int RBRACK = 10;
+  public static final int LBRACE = 11;
+  public static final int RBRACE = 12;
+  public static final int NIL = 13;
+  public static final int COMMA = 14;
+  public static final int DOT = 15;
+  public static final int SEMICOLON = 16;
+  public static final int TYPE_INT = 17;
+  public static final int ASSIGN = 18;
+  public static final int EQ = 19;
+  public static final int LT = 20;
+  public static final int GT = 21;
+  public static final int ARRAY = 22;
+  public static final int CLASS = 23;
+  public static final int EXTENDS = 24;
+  public static final int RETURN = 25;
+  public static final int WHILE = 26;
+  public static final int IF = 27;
+  public static final int NEW = 28;
+  public static final int STRING = 29;
+  public static final int TYPE_STRING = 30;
+
 
 }
