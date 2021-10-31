07c299dbaa2d80d041d679d541cd73966ce3c5c3
harel-weinstock <harel.w.1234@gmail.com>
2021-10-31 15:08:06 +0200

    update

--- ex1/src/Lexer.java
+++ ex1/src/Lexer.java
@@ -1,13 +1,29 @@
 import java.io.FileReader;
+import java.io.IOException;
+
 import java_cup.runtime.Symbol;
 
 public class Lexer {
+    private final FileReader fileReader;
+    private int lineCounter;
+    private int offset;
+    private int offset_from_start_of_file;
     public Lexer(FileReader file_reader) {
+        fileReader = file_reader;
+        lineCounter = 0;
+        offset = 0;
     }
 
-    public Symbol next_token()
-    {
-        return new Symbol(0);
+    public Symbol next_token() throws IOException {
+        char[] buff = new char[1];
+        int result;
+        result = fileReader.read(buff, offset, 1);
+        if(result == -1)
+            return new Symbol(TokenNames.EOF);
+        else
+        {
+            if(buff[0] == '')
+        }
     }
 
     public boolean getLine() {
