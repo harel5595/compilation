6487266641b691c439412224f86034bfeff0b739
unknown <Khashper@mail.tau.ac.il>
2021-10-31 15:18:46 +0200

    cool4

--- ex1/src/Lexer.java
+++ ex1/src/Lexer.java
@@ -16,14 +16,28 @@ public class Lexer {
 
     public Symbol next_token() throws IOException {
         char[] buff = new char[1];
+        String string = "";
         int result;
         result = fileReader.read(buff, offset, 1);
         if(result == -1)
             return new Symbol(TokenNames.EOF);
-        else
+        else if(buff[0] == '(')
         {
-            if(buff[0] == '')
+            return new Symbol(TokenNames.LPAREN);
         }
+        else if(buff[0] == ')')
+        {
+            return new Symbol(TokenNames.RPAREN);
+        }
+        else if(buff[0] == '[')
+        {
+            return new Symbol(TokenNames.LBRACK);
+        }
+        else if(buff[0] == ']')
+        {
+            return new Symbol(TokenNames.LPAREN);
+        }
+        return null;
     }
 
     public boolean getLine() {
--- ex1/src/TokenNames.java
+++ ex1/src/TokenNames.java
@@ -7,6 +7,9 @@ public interface TokenNames {
   public static final int DIVIDE = 4;
   public static final int LPAREN = 5;
   public static final int RPAREN = 6;
+  public static final int LBRACK = 9;
+  public static final int RBRACK = 10;
   public static final int NUMBER = 7;
   public static final int ID = 8;
+
 }
