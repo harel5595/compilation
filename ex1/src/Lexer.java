92850930f5e185a6ac2450fbcb3362d953cdb0cc
harel-weinstock <harel.w.1234@gmail.com>
2021-10-31 16:11:29 +0200

    update

--- ex1/src/Lexer.java
+++ ex1/src/Lexer.java
@@ -1,5 +1,6 @@
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Objects;
 
 import java_cup.runtime.Symbol;
 
@@ -12,6 +13,79 @@ public class Lexer {
         fileReader = file_reader;
         lineCounter = 0;
         offset = 0;
+    }
+
+    private boolean is_end_of_token(char to_check)
+    {
+        if(to_check == '\n' || to_check == ' ' || to_check == ')' ||
+                to_check == '(' || to_check == '{' || to_check == '}'
+                || to_check == '[' || to_check == ']' || to_check == '+'
+                || to_check == '+' || to_check == '-' || to_check == '/'
+                || to_check == '*' || to_check == )
+        {
+
+        }
+    }
+
+    private Symbol check_for_saved_words(String token)
+    {
+        if(Objects.equals(token, "class"))
+        {
+            return new Symbol(TokenNames.CLASS);
+        }
+        else if(Objects.equals(token, "nil"))
+        {
+            return new Symbol(TokenNames.NIL);
+        }
+        else if(Objects.equals(token, "array"))
+        {
+            return new Symbol(TokenNames.ARRAY);
+        }
+        else if(Objects.equals(token, "while"))
+        {
+            return new Symbol(TokenNames.WHILE);
+        }
+        else if(Objects.equals(token, "int"))
+        {
+            return new Symbol(TokenNames.NUMBER);
+        }
+        else if(Objects.equals(token, "extends"))
+        {
+            return new Symbol(TokenNames.EXTENDS);
+        }
+        else if(Objects.equals(token, "return"))
+        {
+            return new Symbol(TokenNames.RETURN);
+        }
+        else if(Objects.equals(token, "new"))
+        {
+            return new Symbol(TokenNames.NEW);
+        }
+        else if(Objects.equals(token, "if"))
+        {
+            return new Symbol(TokenNames.IF);
+        }
+        else if(Objects.equals(token, "string"))
+        {
+            return new Symbol(TokenNames.STRING);
+        }
+        else
+        {
+            return new Symbol(TokenNames.ID, token);
+        }
+    }
+
+    private Symbol next_string_token(char first_char) throws IOException {
+        String token = String.valueOf(first_char);
+        char[] buff = new char[1];
+        int result;
+        result = fileReader.read(buff, offset, 1);
+        while (result > 0 && ((buff[0] >= 'a' && buff[0] <= 'z') || (buff[0] >= '0' && buff[0] <= '9') || (buff[0] >= 'A' && buff[0] <= 'Z')))
+        {
+            token = token.concat(String.valueOf(buff[0]));
+        }
+
+        return check_for_saved_words(token);
     }
 
     public Symbol next_token() throws IOException {
