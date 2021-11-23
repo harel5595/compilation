98a98ce8bb942bc2129c31b7c376b5f78d1794aa
harel-weinstock <harel.w.1234@gmail.com>
2021-11-23 21:04:20 +0200

    update

--- ex2/src/AST/AST_new_exp.java
+++ ex2/src/AST/AST_new_exp.java
@@ -25,6 +25,23 @@ public class AST_new_exp extends  AST_Node{
         this.exp = exp;
         this.type = type;
     }
+    public AST_new_exp(AST_type type)
+    {
+        /******************************/
+        /* SET A UNIQUE SERIAL NUMBER */
+        /******************************/
+        SerialNumber = AST_Node_Serial_Number.getFresh();
+
+        /***************************************/
+        /* PRINT CORRESPONDING DERIVATION RULE */
+        /***************************************/
+        System.out.format("====================== AST New_EXP( type:%s)\n",type);
+
+        /*******************************/
+        /* COPY INPUT DATA NENBERS ... */
+        /*******************************/
+        this.type = type;
+    }
 
     /************************************************/
     /* The printing message for an INT EXP AST node */
@@ -34,14 +51,21 @@ public class AST_new_exp extends  AST_Node{
         /*******************************/
         /* AST NODE TYPE = AST INT EXP */
         /*******************************/
-        System.out.format("AST New_EXP( type:%s, value: %s)\n",type,exp );
-
+        if(exp != null)
+            System.out.format("AST New_EXP( type:%s, value: %s)\n",type,exp );
+        else
+            System.out.format("AST New_EXP( type:%s, value: %s)\n",type);
         /*********************************/
         /* Print to AST GRAPHIZ DOT file */
         /*********************************/
-        AST_GRAPHVIZ.getInstance().logNode(
+        if(exp != null)
+            AST_GRAPHVIZ.getInstance().logNode(
                 SerialNumber,
-                String.format("INT(%s)",exp));
+                String.format("NEW %s(%s)",type, exp));
+        else
+            AST_GRAPHVIZ.getInstance().logNode(
+                    SerialNumber,
+                    String.format("NEW %s",type));
     }
 
 
