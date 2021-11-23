eb34b454c66cc3f3f3bf03ae1207d96d9edbb213
ionikha <Khashper@mail.tau.ac.il>
2021-11-23 22:09:08 +0200

    added print for exp STR

--- ex2/src/AST/AST_EXP_STR.java
+++ ex2/src/AST/AST_EXP_STR.java
@@ -7,4 +7,22 @@ public class AST_EXP_STR extends AST_EXP{
         SerialNumber = AST_Node_Serial_Number.getFresh();
         this.content = content;
     }
+
+    public void PrintMe()
+    {
+        /*******************************/
+        /* AST NODE TYPE = AST INT EXP */
+        /*******************************/
+        System.out.format("AST string : %s \n", content);
+
+        /*********************************/
+        /* Print to AST GRAPHIZ DOT file */
+        /*********************************/
+        AST_GRAPHVIZ.getInstance().logNode(
+                SerialNumber,
+                String.format("String %s", content));
+    }
+
+
+
 }
