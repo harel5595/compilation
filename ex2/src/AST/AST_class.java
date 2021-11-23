ca31e80951749ed962b6d14134c80b8e6d4c0bc6
ionikha <Khashper@mail.tau.ac.il>
2021-11-23 21:21:25 +0200

    added alot of things

--- ex2/src/AST/AST_class.java
+++ ex2/src/AST/AST_class.java
@@ -1,4 +1,77 @@
 package AST;
 
-public class AST_class extends AST_dec{
+public class AST_class extends AST_dec {
+    /***************/
+    /*  var := exp */
+    /***************/
+    public String ID;
+    public List<AST_dec> fields;
+
+    /*******************/
+    /*  CONSTRUCTOR(S) */
+
+    /*******************/
+
+
+    public AST_class(String ID, List<AST_dec> fields) {
+        /******************************/
+        /* SET A UNIQUE SERIAL NUMBER */
+        /******************************/
+        SerialNumber = AST_Node_Serial_Number.getFresh();
+
+        /***************************************/
+        /* PRINT CORRESPONDING DERIVATION RULE */
+        /***************************************/
+        System.out.format("====================== var -> var DOT ID( %s )\n", ID);
+
+        /*******************************/
+        /* COPY INPUT DATA NENBERS ... */
+        /*******************************/
+        this.ID = ID;
+        this.fields = fields;
+    }
+
+    /*************************************************/
+    /* The printing message for a field var AST node */
+
+    /*************************************************/
+    public void PrintMe() {
+        /*********************************/
+        /* AST NODE TYPE = AST FIELD VAR */
+        /*********************************/
+        System.out.print("AST NODE Class ID(%s)\n", ID);
+
+        /***************************************/
+        /* PRINT Node to AST GRAPHVIZ DOT file */
+        /***************************************/
+        AST_GRAPHVIZ.getInstance().logNode(
+                SerialNumber,
+                String.format("class\nID\n...->%s", ID));
+
+
+        /**********************************************/
+        /* RECURSIVELY PRINT VAR, then FIELD NAME ... */
+        /**********************************************/
+        for (AST_dec field :
+                fields) {
+            System.out.print("AST NODE FIELD \n");
+            field.PrintMe();
+
+
+
+            /****************************************/
+            /* PRINT Edges to AST GRAPHVIZ DOT file */
+            /****************************************/
+            AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, field.SerialNumber);
+
+
+
+
+
+        }
+
+
+    }
+
+
 }
