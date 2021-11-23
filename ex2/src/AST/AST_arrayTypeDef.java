926f725def80c49e1848fee14ecca12cab48607e
Vathys3 <yuval.zahavyakir1@gmail.com>
2021-11-23 21:57:56 +0200

    added AST_arrayTypeDef, fixed funcDec and arrayTypeDef in CUP

---
+++ ex2/src/AST/AST_arrayTypeDef.java
@@ -0,0 +1,50 @@
+package AST;
+
+public class AST_arrayTypeDef extends AST_Node {
+	public String arrayName;
+	public AST_type t;
+	
+	
+	public AST_arrayTypeDef(String arrayName, AST_type t) {
+		
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		System.out.print("====================== exp -> exp BINOP exp\n");
+
+		/*******************************/
+		/* COPY INPUT DATA MEMBERS ... */
+		/*******************************/
+		this.arrayName = arrayName;
+		this.t = t;
+	}
+
+	public void PrintMe()
+	{
+		
+		System.out.print("AST NODE ARRAY TYPE DEF\n");
+
+		/**************************************/
+		/* RECURSIVELY PRINT type ... */
+		/**************************************/
+		if (t != null) t.PrintMe();
+		
+		/***************************************/
+		/* PRINT Node to AST GRAPHVIZ DOT file */
+		/***************************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			String.format("ARRAYNAME(%s)",arrayName));
+		
+		/****************************************/
+		/* PRINT Edges to AST GRAPHVIZ DOT file */
+		/****************************************/
+		if (t  != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,t.SerialNumber);
+	}
+}
+
