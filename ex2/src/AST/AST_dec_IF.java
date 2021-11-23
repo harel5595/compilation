e38b8c90745dd1fd515faf3a653415026e45b836
ionikha <Khashper@mail.tau.ac.il>
2021-11-23 21:50:48 +0200

    ast dec if

--- ex2/src/AST/AST_dec_IF.java
+++ ex2/src/AST/AST_dec_IF.java
@@ -3,12 +3,12 @@ package AST;
 public class AST_dec_IF extends AST_dec
 {
 	public AST_EXP cond;
-	public AST_Program body;
+	public List<AST_dec> body;
 
 	/*******************/
 	/*  CONSTRUCTOR(S) */
 	/*******************/
-	public AST_dec_IF(AST_EXP cond, AST_Program body)
+	public AST_dec_IF(AST_EXP cond, List<AST_dec> body)
 	{
 		this.cond = cond;
 		this.body = body;
